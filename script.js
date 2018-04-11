93d405d00341cb44c42c1956422c7796706b83bd
Paulina Chojnowska <antaria@vp.pl>
2018-04-11 16:58:48 +0200

    Finding position of elements in body

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 window.addEventListener("hashchange", function () {
     window.scrollTo(window.scrollX, window.scrollY - 100);
 });
+
+var featuresSection = document.getElementById("features");
+var featuresPosition = document.getElementById("features").getBoundingClientRect();
+
+
+function myFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 50) {
+        document.getElementById("features").className = "animated bounceOutLeft";
+    } else {
+        document.getElementById("features").className = "animated bounceOutLeft";
+    }
+}
+
+
+
+// window.addEventListener("scroll", function () {
+//     alert("Hello");
+//
+//
+//     if (featuresSection.scrollTop > featuresPosition.top) {
+//         alert("Hello");
+//     }
+// });
+//
+//
+// console.log(featuresPosition.top, featuresPosition.right, featuresPosition.bottom, featuresPosition.left);
