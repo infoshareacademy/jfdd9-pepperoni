2795986f7016102de1a52717804d903c2720f959
Patryk Czupryn <your.paczu1991@gmail.com>
2018-04-17 16:58:44 +0200

    new animation new background

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ var score = 0;
 var arrayWithSlots = [];
 var positionPerson;
 var personWidth = 40;
-var civilianProbability = 0.4;
+var civilianProbability = 0.1;
 var gameInterval;
 var civiliansKilled = 0;
 
@@ -39,21 +39,35 @@ function generatePerson(personClass) {
     arrayWithSlots.splice(arrayWithSlots.indexOf(positionPerson), 1);
 }
 
-
+var animateIterator = 1;
 gameBoard.addEventListener("click", function (event) {
     var clickedElement = event.target;
+    if (clickedElement.classList.contains('dead')) {
+        return;
+    }
     if (clickedElement.classList.contains("person")) {
-        gameBoard.removeChild(clickedElement);
-        arrayWithSlots.push(parseInt(clickedElement.style.left));
-        if (clickedElement.classList.contains("gangster")) {
-            score += 100;
-        } else if (clickedElement.classList.contains("civilian")) {
-            score -= 100;
-            civiliansKilled += 1;
-            if (civiliansKilled === 3) {
-                finishGame();
+        clickedElement.classList.add('dead');
+        var animInterval = setInterval(function () {
+            clickedElement.style.backgroundImage = 'url("./game_images/cut1/GunOne' + (animateIterator++) + '.png")';
+            //animate
+
+           if (animateIterator === 8) {
+                animateIterator = 1;
+                clearInterval(animInterval);
+                gameBoard.removeChild(clickedElement);
+                arrayWithSlots.push(parseInt(clickedElement.style.left));
+                if (clickedElement.classList.contains("gangster")) {
+                    score += 100;
+                } else if (clickedElement.classList.contains("civilian")) {
+                    score -= 100;
+                    civiliansKilled += 1;
+                    if (civiliansKilled === 3) {
+                        finishGame();
+                    }
+                }
             }
-        }
+        }, 100);
+
     }
     scoreSection.innerText = "Score: " + score;
 });
@@ -87,7 +101,8 @@ function update() {
 
 
 function runGame() {
-    gameInterval = setInterval(update, 1100);
+    update()
+    gameInterval = setInterval(update, 5000);
 }
 
 function finishGame() {
