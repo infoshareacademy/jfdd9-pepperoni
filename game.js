fa452031c0514e537dadcef051704078ea33e536
Patryk Czupryn <your.paczu1991@gmail.com>
2018-04-20 16:40:00 +0200

    added gangsters and animations added css class gangster1-4

--- game.js
+++ game.js
@@ -13,13 +13,14 @@ var dif_level = 0; // 0 - novice, 1 - brutal
 var ammoHTML = document.getElementsByClassName("ammo");
 var ammo = Array.prototype.slice.call(ammoHTML);
 var ammunition = 5;
+var classes = ["gangster1", "gangster2", "gangster3", "gangster4"];
 
 //Creating array of available pixel slots. The function accepts person's width as an argument.
 var boardGameWidth = gameBoard.offsetWidth;
 
 function reload() {
     ammunition = 5;
-    ammo.forEach(function(bullet){
+    ammo.forEach(function (bullet) {
         if (bullet.classList.contains("transparent")) {
             bullet.classList.remove("transparent");
             bullet.classList.add("opaque")
@@ -28,17 +29,17 @@ function reload() {
 }
 
 function removeBullet() {
-    ammo[ammunition-1].classList.remove("opaque");
-    ammo[ammunition-1].classList.add("transparent");
+    ammo[ammunition - 1].classList.remove("opaque");
+    ammo[ammunition - 1].classList.add("transparent");
     if (ammunition > 0) {
-        ammunition = ammunition-1;
+        ammunition = ammunition - 1;
     }
 }
 
 window.addEventListener('keydown', function (event) {
-console.log(event.code);
+    console.log(event.code);
     if (event.code === 'KeyR') {
-    reload();
+        reload();
     }
 });
 
@@ -52,6 +53,7 @@ function recreateArrayWithSlots(personWidth) {
         return index * personWidth
     });
 }
+
 ////
 
 function generatePerson(personClass) {
@@ -70,6 +72,7 @@ function generatePerson(personClass) {
 
     arrayWithSlots.splice(arrayWithSlots.indexOf(positionPerson), 1);
 }
+
 function shoot(event) {
     var clickedElement = event.target;
 
@@ -86,7 +89,11 @@ function shoot(event) {
 
     if (clickedElement.classList.contains("person")) {
         clickedElement.classList.add('dead');
-        if (clickedElement.classList.contains("gangster1" || "gangster2" || "gangster3" || "gangster4")) {
+
+
+        if (classes.some(function (className) {
+                return clickedElement.classList.contains(className)
+            })) {
             score += 1;
         } else if (clickedElement.classList.contains("civilian")) {
             civiliansKilled += 1;
@@ -95,6 +102,8 @@ function shoot(event) {
                 return;
             }
         }
+
+        var animateIterator = 1;
 
         var animInterval = setInterval(function () {
             if (clickedElement.classList.contains("gangster1")) {
@@ -127,13 +136,12 @@ function shoot(event) {
                 arrayWithSlots.push(parseInt(clickedElement.style.left));
             }
 
-        }, 80);
+        }, 100);
 
     }
     scoreSection.innerText = "Score: " + score;
     scoreCiviliansSection.innerText = "Civilians killed: " + civiliansKilled;
 }
-
 
 
 function clearBoard() {
@@ -144,23 +152,9 @@ function clearBoard() {
 
 function createRandomPerson() {
     if (Math.random() > civilianProbability) {
-        generatePerson("gangster1" || "gangster2"|| "gangster3" || "gangster4") //&& randomNum <= 4;
-    if (Math.floor((Math.random() * 4) +1 ) === 1){
-        generatePerson("gangster1")
-    }
-        if (Math.floor((Math.random() * 4) +1 ) === 2){
-            generatePerson("gangster2")
-        }
-        if (Math.floor((Math.random() * 4) +1 ) === 3){
-            generatePerson("gangster3")
-        }
-        if (Math.floor((Math.random() * 4) +1 ) === 4){
-            generatePerson("gangster4")
-        }
+        generatePerson(classes[Math.floor((Math.random() * 4))]);
 
-    }
-
-    else {
+    } else {
         generatePerson('civilian')
     }
 }
@@ -168,34 +162,34 @@ function createRandomPerson() {
 function update() {
     clearBoard();
     recreateArrayWithSlots(personWidth);
-    for (var i = 0; i<5; i++) {
+    for (var i = 0; i < 5; i++) {
         createRandomPerson();
     }
 }
 
-function welcomeScreen(){
+function welcomeScreen() {
     gameBoard.innerHTML = '' +
         '<div>' +
         '<h2>Try the GangBook game</h2>' +
         '<p>Kill as many gansgters as possible.<br/>But beware! Spare the civilians! Don\'t kill more than two.</p>' +
-        '<section>'+
+        '<section>' +
         '<label class="container">Novice <input type="radio" name="dif-level" value="0" checked /><span class="checkmark"></span></label>' +
         '<label class="container">Brutal <input type="radio" name="dif-level" value="1" /><span class="checkmark"></span></label>' +
-        '</section>'+
+        '</section>' +
         '<button class="button-game" onclick="runGame()">START</button>' +
         '</div>';
 }
 
-function gameOverScreen(){
+function gameOverScreen() {
     gameBoard.innerHTML = '' +
         '<div>' +
         '<h2>GAME OVER</h2>' +
         '<p>You killed ' + score + ' gangster(s).</p>' +
         '<p>But you also killed 3 civilians.</p>' +
-        '<section>'+
+        '<section>' +
         '<label class="container">Novice <input type="radio" name="dif-level" value="0" checked /><span class="checkmark"></label>' +
         '<label class="container">Brutal <input type="radio" name="dif-level" value="1" /><span class="checkmark"></label>' +
-        '</section>'+
+        '</section>' +
         '<button class="button-game" onclick="runGame()">RESTART GAME</button>' +
         '</div>';
 }
@@ -206,14 +200,14 @@ function runGame() {
     resetScores();
     update();
     reload();
-    gameInterval = setInterval(update, dif_level === '1' ? 1000 : 3000);
+    gameInterval = setInterval(update, dif_level === '1' ? 4000 : 8000);
 }
 
 function finishGame() {
     gameBoard.removeEventListener("mousedown", shoot);
-    ammo.forEach(function(bullet){
-            bullet.classList.remove("opaque");
-            bullet.classList.add("transparent")
+    ammo.forEach(function (bullet) {
+        bullet.classList.remove("opaque");
+        bullet.classList.add("transparent")
     });
     clearBoard();
     window.clearInterval(gameInterval);
